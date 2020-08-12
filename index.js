d788331e78f124ab234ec754f1e0185959b3b85c
Federico Piaggio <federicopiaggio26@gmail.com>
2020-08-12 01:29:28 -0300

    fix to mobile

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
-function PlaySound(e) {
-  const audio = document.querySelector(`audio[data-key = "${e.keyCode}"]`);
-  const key = document.querySelector(`.key[data-key = "${e.keyCode}"]`);
+function Playsound(audio, key) {
   if (!audio) return;
   audio.currentTime = 0;
   audio.play();
   key.classList.add("playing");
 }
 
-window.addEventListener("keydown", PlaySound);
+function keyDown(e) {
+  const audio = document.querySelector(`audio[data-key = "${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key = "${e.keyCode}"]`);
+  Playsound(audio, key);
+}
+const keys = document.querySelectorAll(".key");
+window.addEventListener("keydown", keyDown);
+
+function click() {
+  const keyNumb = this.getAttribute("data-key");
+  const audio = document.querySelector(`audio[data-key = "${keyNumb}"]`);
+  Playsound(audio, this);
+}
+
+keys.forEach((e) => {
+  e.addEventListener("click", click);
+});
 
 function removeTransition(e) {
   if (e.propertyName !== "transform") return;
   this.classList.remove("playing");
 }
-
-const keys = document.querySelectorAll(".key");
 
 keys.forEach((key) => {
   key.addEventListener("transitionend", removeTransition);
